f5e9246afc65572b5d1f9274ee6323ea65be29ed
skypotato <hunsooski@naver.com>
2017-12-20 15:11:09 +0900

    선택목록 UI 작성

---
+++ app/src/main/java/org/skypotato/love_or_not/SelectListActivity.java
@@ -0,0 +1,13 @@
+package org.skypotato.love_or_not;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SelectListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select_list);
+    }
+}
--- app/src/main/java/org/skypotato/love_or_not/SettingActivity.java
+++ app/src/main/java/org/skypotato/love_or_not/SettingActivity.java
@@ -5,7 +5,11 @@ import android.content.SharedPreferences;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 import com.tsengvn.typekit.TypekitContextWrapper;
 
@@ -13,25 +17,72 @@ public class SettingActivity extends AppCompatActivity {
 
     SharedPreferences sharedPref;
 
+    RadioGroup rgStartSetting;
+    RadioButton rbStartSetting_01;
+    RadioButton rbStartSetting_02;
+
+    Button btBack;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
-
-        RadioButton rbStartSetting_01 = (RadioButton) findViewById(R.id.rbStartSetting_01);
-        RadioButton rbStartSetting_02 = (RadioButton) findViewById(R.id.rbStartSetting_02);
-
-        sharedPref = getSharedPreferences(getString(R.string.preference_file_key), MODE_PRIVATE);
-        int defaultValue = 1; // default 값은 시작
-        long settingRBValue = sharedPref.getInt(getString(R.string.setting_001), defaultValue);
-
-        if(settingRBValue == 1){
-            rbStartSetting_01.isChecked();
-        }else if(settingRBValue == 2){
-            rbStartSetting_02.isChecked();
-        }
-
+        settingView(); // 화면구성
     }
+    // 화면구성
+    public void settingView(){
+        Log.d("start",":::::settingView()...Start");
+
+        btBack = (Button) findViewById(R.id.btBack);
+        rgStartSetting = (RadioGroup) findViewById(R.id.rgStartSetting);
+        rbStartSetting_01 = (RadioButton) findViewById(R.id.rbStartSetting_01);
+        rbStartSetting_02 = (RadioButton) findViewById(R.id.rbStartSetting_02);
+
+        rgStartSetting.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int checkedId) {
+                SharedPreferences pref = getSharedPreferences(getString(R.string.preference_file_key), MODE_PRIVATE);
+                SharedPreferences.Editor editor = pref.edit();
+
+                switch (checkedId) {
+                    case R.id.rbStartSetting_01:
+                            editor.putInt(getString(R.string.setting_001), 1);
+                        break;
+
+                    case R.id.rbStartSetting_02:
+                            editor.putInt(getString(R.string.setting_001), 2);
+                        break;
+                }
+
+                editor.commit();
+            }
+        });
+
+        btBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onBackPressed();
+            }
+        });
+
+        Log.d("start",":::::settingView()...End");
+    }
+
+    public void getStartSetting(){
+        Log.d("start",":::::getStartSetting()...Start");
+        sharedPref = getSharedPreferences(getString(R.string.preference_file_key), MODE_PRIVATE);
+        long settingRBValue = sharedPref.getInt(getString(R.string.setting_001), 1);
+
+        if (settingRBValue == 1) {
+            rbStartSetting_01.setChecked(true);
+            rbStartSetting_02.setChecked(false);
+        } else if (settingRBValue == 2) {
+            rbStartSetting_01.setChecked(false);
+            rbStartSetting_02.setChecked(true);
+        }
+        Log.d("start",":::::getStartSetting()...End");
+    }
+
 
     /* 폰트적용 */
     @Override
