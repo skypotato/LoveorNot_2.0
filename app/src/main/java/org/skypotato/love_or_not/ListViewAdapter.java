451290e2dce7021fd4a6758b5686d3f4245af660
skypotato <hunsooski@naver.com>
2017-12-29 15:28:31 +0900

    커스텀 리스트 뷰 (Footer 추가하기 전)

--- app/src/main/java/org/skypotato/love_or_not/ListViewAdapter.java
+++ app/src/main/java/org/skypotato/love_or_not/ListViewAdapter.java
@@ -1,6 +1,7 @@
 package org.skypotato.love_or_not;
 
 import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -40,7 +41,8 @@ public class ListViewAdapter extends BaseAdapter {
     }
 
     @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
+    public View getView(final int position, View convertView, ViewGroup parent) {
+        Log.d("start",":::::getView()...Start");
         ViewHolder viewHolder;
         if(convertView == null){
             viewHolder = new ViewHolder();
@@ -49,7 +51,14 @@ public class ListViewAdapter extends BaseAdapter {
             convertView = inflater.inflate(R.layout.listview_item, null);
 
             viewHolder.itemText = (EditText)convertView.findViewById(R.id.itemText);
-            viewHolder.btDelte = (Button)convertView.findViewById(R.id.btDelte);
+            viewHolder.btDelete = (Button)convertView.findViewById(R.id.btDelte);
+
+            viewHolder.btDelete.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    removeItem(position);
+                }
+            });
 
             convertView.setTag(viewHolder);
         }
@@ -60,24 +69,31 @@ public class ListViewAdapter extends BaseAdapter {
         ListData data = listData.get(position);
 
         viewHolder.itemText.setText(data.strItem);
+        Log.d("start",":::::getView()...End");
         return convertView;
     }
 
     public  void addItem(ListData data){
+        Log.d("start",":::::addItem()...Start");
         listData.add(data);
         dataChange();
+        Log.d("start",":::::addItem()...End");
     }
 
     public void removeItem(int position){
+        Log.d("start",":::::removeItem()...Start");
         listData.remove(position);
         dataChange();
+        Log.d("start",":::::removeItem()...End");
     }
     public void dataChange(){
+        Log.d("start",":::::dataChange()...Start");
         this.notifyDataSetChanged();
+        Log.d("start",":::::dataChange()...End");
     }
 }
 
 class ViewHolder {
     public EditText itemText;
-    public Button btDelte;
+    public Button btDelete;
 }
--- app/src/main/java/org/skypotato/love_or_not/SelectListActivity.java
+++ app/src/main/java/org/skypotato/love_or_not/SelectListActivity.java
@@ -1,10 +1,18 @@
 package org.skypotato.love_or_not;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ListView;
 
+import com.tsengvn.typekit.TypekitContextWrapper;
+
 public class SelectListActivity extends AppCompatActivity {
+
+    Button btBack;
 
     ListView listView;
     ListViewAdapter listViewAdapter;
@@ -13,9 +21,14 @@ public class SelectListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_list);
+
+        settingView();
     }
 
     public void settingView(){
+        Log.d("start",":::::settingView()...Start");
+
+        btBack = (Button) findViewById(R.id.btBack);
         listView = (ListView)findViewById(R.id.listView);
         listViewAdapter = new ListViewAdapter(this);
 
@@ -26,5 +39,19 @@ public class SelectListActivity extends AppCompatActivity {
             data.strItem = "리스트 "+i;
             listViewAdapter.addItem(data);
         }
+
+        btBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onBackPressed();
+            }
+        });
+        Log.d("start",":::::settingView()...End");
+    }
+
+    /* 폰트적용 */
+    @Override
+    protected void attachBaseContext(Context newBase) {
+        super.attachBaseContext(TypekitContextWrapper.wrap(newBase));
     }
 }
